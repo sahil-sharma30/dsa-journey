3fb5e94de5d1113c6244e5a52d962094fef9ee30
Sahil Sharma <sahilsharma30112005@gmail.com>
2026-07-16 21:57:53 +0530

    Add sorted array check code

--- 12_Recursion_and_Backtracking/02_recursion_intermediate.cpp
+++ 12_Recursion_and_Backtracking/02_recursion_intermediate.cpp
@@ -27,15 +27,39 @@ int fib(int n)
         a = b;       // Shift 'a' forward
         b = ans;     // Shift 'b' forward
     }
-    
+
     return ans;
+}
+
+bool isSorted(int arr[], int n)
+{
+    // Base Cases
+    if (n == 0 || n == 1)
+    {
+        return true;
+    }
+
+    // Defect Check
+    if (arr[0] > arr[1])
+    {
+        return false;
+    }
+    else
+    {
+        return isSorted(arr + 1, n - 1);
+    }
 }
 
 int main()
 {
     int n = 7;
 
+    int arrsize = 5;
+    int arr[] = {1, 2, 10, 4, 5};
+
     cout << "Fibonacci of " << n << " is: " << fib(n) << endl;
+
+    cout << isSorted(arr, arrsize);
 
     return 0;
 }
