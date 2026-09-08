470f6d77421c731db969391f878874f39fc395ca
Sahil Sharma <sahilsharma30112005@gmail.com>
2026-09-08 21:59:59 +0530

    Complete doubly linked list core architecture

---
+++ 09_Linked_Lists/04_doubly_linked_list.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+
+public:
+    Node(int val)
+    {
+        data = val;
+        next = prev = NULL;
+    }
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = tail = NULL;
+    }
+
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
+        if (tail == NULL)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void pop_front()
+    {
+        if (head == NULL)
+        {
+            cout << "No Node to pop!" << endl;
+        }
+        else if (head == tail)
+        {
+            Node *temp = head;
+            head = tail = NULL;
+            delete temp;
+        }
+        else
+        {
+            Node *temp = head;
+            head = head->next;
+            head->prev = NULL;
+            delete temp;
+        }
+    }
+
+    void pop_back()
+    {
+        if (tail == NULL)
+        {
+            cout << "No Node to pop!" << endl;
+        }
+        else if (head == tail)
+        {
+            Node *temp = tail;
+            head = tail = NULL;
+            delete temp;
+        }
+        else
+        {
+            Node *temp = tail;
+            tail = tail->prev;
+            tail->next = NULL;
+            delete temp;
+        }
+    }
+
+    void print_list()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    void print_reverse()
+    {
+        Node *temp = tail;
+        while (temp != NULL)
+        {
+            cout << temp->data << " ";
+            temp = temp->prev;
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    List mylist;
+
+    mylist.push_back(20);
+    mylist.push_back(30);
+    mylist.push_front(10);
+    mylist.push_front(5);
+
+    mylist.print_list();
+    mylist.print_reverse();
+
+    mylist.pop_front();
+    mylist.pop_back();
+
+    mylist.print_list();
+
+    mylist.pop_front();
+    mylist.pop_front();
+    mylist.pop_front();
+
+    mylist.print_list();
+
+    return 0;
+}
